c07b407a3caa2c8de19bc1e33f64ecc85456a54a
Marta Makowska <silent.aqua@gmail.com>
2023-03-17 08:22:41 +0100

    AdventOfCode 2015 Day 7 Part 1

--- 2015/Day7/2015-day7-1.py
+++ 2015/Day7/2015-day7-1.py
@@ -1,10 +1,52 @@
-import Gate
+import operator
 
 file = open("input.txt", "r")
-linie = file.read().splitlines()
+lines = file.read().splitlines()
 
-gates = []
+ops = {
+    'AND': operator.and_,
+    'OR': operator.or_,
+    'RSHIFT': operator.rshift,
+    'LSHIFT': operator.lshift,
+    'NOT': operator.not_
+}
 
-for i in range(len(linie)):
-    linie[i] = linie[i].split(' ')
-    print(linie[i])
+gates = {}
+
+for i in range(len(lines)):
+    lines[i] = lines[i].split(' ')
+    gates[lines[i][-1]] = -1
+
+
+def find_gate(name: str) -> int:
+    signal_value = -1
+
+    if gates[name] >= 0:
+        signal_value = gates[name]
+        return signal_value
+
+    for i in range(len(lines)):
+        if lines[i][-1] == name:
+            if len(lines[i]) == 3:
+                if lines[i][0].isnumeric():
+                    gates[name] = int(lines[i][0])
+                    signal_value = gates[name]
+                    break
+                else:
+                    signal_value = find_gate(lines[i][0])
+                    gates[name] = signal_value
+                    break
+            elif len(lines[i]) == 4:
+                signal_value = ~find_gate(lines[i][1])
+                signal_value = signal_value & 0xffff
+                gates[name] = signal_value
+                break
+            elif len(lines[i]) == 5:
+                gates[name] = ops[lines[i][1]](find_gate(lines[i][0]), find_gate(lines[i][2]))
+                signal_value = gates[name]
+                break
+
+    return signal_value
+
+
+print(find_gate('a'))
