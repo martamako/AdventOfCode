e0a171fa03e3c9730b67eab3d426f164f6c94ec2
Marta Makowska <silent.aqua@gmail.com>
2023-03-20 09:13:23 +0100

    AdventOfCode 2015 Day 10 Part 2

---
+++ 2015/Day10/2015-day10-2.py
@@ -0,0 +1,25 @@
+
+
+def look_and_day(text: str) -> str:
+    new_string = ""
+    digit = text[0]
+    numberOfCopies = 0
+
+    for i in range(len(text)):
+        if digit != text[i]:
+            new_string += str(numberOfCopies) + digit
+            numberOfCopies = 1
+            digit = text[i]
+        else:
+            numberOfCopies += 1
+
+    new_string += str(numberOfCopies) + digit
+
+    return new_string
+
+
+text = "1113222113"
+for i in range(50):
+    text = look_and_day(text)
+
+print(len(text))
