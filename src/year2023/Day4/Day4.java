453d612939965f76fe1eebdbf9a8821f2067c468
Marta Makowska <silent.aqua@gmail.com>
2023-12-08 22:09:07 +0100

    Day 4 Part 2

--- src/year2023/Day4/Day4.java
+++ src/year2023/Day4/Day4.java
@@ -13,26 +13,47 @@ public class Day4 {
     public static void main(String[] args) throws IOException {
         List<String> read = Files.readAllLines(path);
         int sum = 0;
+        int power = 0;
+        int index = 0;
         for(String line: read){
-            line = line.replaceAll("[ ]+", " ").strip();
-
-            String[] parts = line.split(": ");
-            parts = parts[1].split(" [|] ");
-            String[] part1 = parts[0].split(" ");
-
-            int[] winningNumbers = Arrays.stream(part1).mapToInt(Integer::parseInt).toArray();
-            Integer[] what = Arrays.stream( winningNumbers ).boxed().toArray( Integer[]::new );
-            String[] part2 = parts[1].split(" ");
-
-            int[] myNumbers = Arrays.stream(part2).mapToInt(Integer::parseInt).toArray();
-            Integer[] ever = IntStream.of( myNumbers ).boxed().toArray( Integer[]::new );
-            Integer[] intersection = Arrays.stream(ever)
-                    .distinct()
-                    .filter(x -> Arrays.asList(what).contains(x))
-                    .toArray(Integer[]::new);
-
+            Integer[] intersection = getIntersection(line);
             sum += (int) Math.pow(2.0, intersection.length - 1);
+            power += getNumberOfCard(read, index) + 1;
+            index++;
         }
         System.out.println(sum);
+        System.out.println(power);
+    }
+
+    static int getNumberOfCard(List<String> lines, int number){
+        if(number >= lines.size()){
+            return 0;
+        }
+        int sum = 0;
+        String line = lines.get(number);
+        Integer[] intersection = getIntersection(line);
+        for(int i=number+1; i <= number + intersection.length; i++){
+             sum += getNumberOfCard(lines, i) + 1;
+        }
+        return sum;
+    }
+
+    static Integer[] getIntersection(String line){
+        line = line.replaceAll("[ ]+", " ").strip();
+        String[] parts = line.split(": ");
+        parts = parts[1].split(" [|] ");
+        String[] part1 = parts[0].split(" ");
+
+        int[] winningNumbers = Arrays.stream(part1).mapToInt(Integer::parseInt).toArray();
+        Integer[] what = Arrays.stream( winningNumbers ).boxed().toArray( Integer[]::new );
+        String[] part2 = parts[1].split(" ");
+
+        int[] myNumbers = Arrays.stream(part2).mapToInt(Integer::parseInt).toArray();
+        Integer[] ever = IntStream.of( myNumbers ).boxed().toArray( Integer[]::new );
+
+        return Arrays.stream(ever)
+                .distinct()
+                .filter(x -> Arrays.asList(what).contains(x))
+                .toArray(Integer[]::new);
     }
 }
